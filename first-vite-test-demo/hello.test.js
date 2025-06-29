b7f513cdec369192e911b7695f811a46f54e1e46
ali mokhtari <alimokhtari7902@gmail.com>
2025-06-30 00:45:51 +0330

    chore: grouping tests

--- first-vite-test-demo/hello.test.js
+++ first-vite-test-demo/hello.test.js
@@ -1,8 +1,31 @@
-import { expect, test } from 'vitest';
-import { sumMultiNumber } from '.';
+import { describe, expect, test } from 'vitest';
+import { subtractOfMultiNumber, sumMultiNumber } from '.';
 
+// test syntax
 // test('test name', test function)
 
-test('testSumOfMultiNumber', () => {
-  expect(sumMultiNumber(1,2,4)).toEqual(7)
+describe('summation group', ()=> {
+  test('testSumOfMultiNumber', () => {
+    expect(sumMultiNumber(1, 2)).toEqual(3);
+  });
+  
+  test('testSumOfMultiNumber', () => {
+    expect(sumMultiNumber(1, 2, 3)).toEqual(6);
+  });
+  
+  test('testSumOfMultiNumber', () => {
+    expect(sumMultiNumber(1, 2, 3, 4)).toEqual(10);
+  });
 })
+
+
+
+describe('subtract group', () => {
+  test('subtractOfMultiNumber', () => {
+    expect(subtractOfMultiNumber(1, 1)).toEqual(0);
+  });
+
+  test('subtractOfMultiNumber', () => {
+    expect(subtractOfMultiNumber(10, 5, 2)).toEqual(3);
+  });
+});
--- first-vite-test-demo/index.js
+++ first-vite-test-demo/index.js
@@ -1,3 +1,7 @@
 export const sumMultiNumber = (...numbers) => {
   return numbers.reduce((a, b) => a + b, 0);
 };
+
+export const subtractOfMultiNumber = (...numbers) => {
+  return numbers.reduce((a, b) => a - b);
+};
